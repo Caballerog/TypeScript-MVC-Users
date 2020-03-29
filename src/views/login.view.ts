e013791f01d430428c9338a320ba3cd920f47ae1
coder1495 <lotherington.sarah@gmail.com>
2020-03-28 19:42:08 -0700

    Example using HTML for login page

--- src/views/login.view.ts
+++ src/views/login.view.ts
@@ -4,40 +4,36 @@ interface Input {
 }
 export class LoginView {
     private app: HTMLElement;
-    private form: HTMLElement;
-    private submitButton: HTMLElement;
-    private inputEmail: HTMLInputElement;
-    private inputPassword: HTMLInputElement;
-    private title: HTMLElement;
 
     constructor() {
-        this.app = document.querySelector('#root');
+        this.app = document.getElementById('root');
 
-        this.form = document.createElement('form');
-        this.inputEmail =
-            Object.assign(document.createElement('input'), 
-                { type: 'text', placeholder: 'Email' });
-        this.inputPassword =
-            Object.assign(document.createElement('input'), 
-                { type: 'password', placeholder: 'Password' });
-        this.submitButton = 
-            Object.assign(document.createElement('button'), 
-                { type: 'submit', textContent: 'Login' });
-        this.form.append(
-            this.inputEmail, 
-            this.inputPassword, 
-            this.submitButton);
-
-        this.title = document.createElement('h1');
-        this.title.textContent = 'Login';
-        this.app.append(this.title, this.form);    
-
-        this.form.addEventListener('submit', event => {
-            event.preventDefault();
-            const { origin, pathname } = location;
-            location.replace(origin+pathname+'?page=signup');
+        const html = `
+            <form id='loginForm' method='post'>
+                <h1>Login</h1>
+                <div>
+                    <input name='email' type='text' placeholder='Email' />
+                </div>
+                <div>
+                    <input name='password' type='password' placeholder='Password' />
+                </div>
+                <div>
+                    <button type='submit'>Login</button>
+                </div>
+            </form>
+        `;
+        this.app.innerHTML = html;
+        
+        document.getElementById('loginForm')
+            .addEventListener('submit', event => {
+                //event.preventDefault(); -- actually post is handy, no need for ajax call
+                const { origin, pathname } = location;
+                setTimeout(()=>{ 
+                    // timeout is temporary hack pending server auth implementation
+                    location.replace(origin+pathname+'?page=signup');
+                })
         });
-            
+
     //    this._temporaryAgeText = '';
     //    this._initLocalListeners();
     }
